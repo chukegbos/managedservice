2caa75d7df6537dc89d003e42e868eb9a7eadabd
Emeka Ohaeri <EmekaOH@armpension.com>
2024-06-26 00:13:17 +0100

    mobile view

--- src/views/private/PrivatePage.vue
+++ src/views/private/PrivatePage.vue
@@ -21,7 +21,7 @@
                 <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
                     aria-expanded="false">
-                    {{ loggedInUser["username"] }} || {{ timeoutInMS }} || {{ formattedCountdown }}
+                    {{ loggedInUser["username"] }}
                   </a>
                   <ul class="dropdown-menu">
                     <li>
