48fce4326fd97802b50e6cf1f4129825bd8e3dea
Emeka Ohaeri <EmekaOH@armpension.com>
2024-07-30 13:50:17 +0100

    Upddate

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -320,7 +320,7 @@
                   </li>
                   <li>
                     <router-link
-                      to="#"
+                      to="/supply-management"
                       v-ripple
                       class="text-decoration-none flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple"
                     >
